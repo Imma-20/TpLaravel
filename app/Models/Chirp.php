f52ef2b19f28a29ae01e8cddf6057d006cf5ee37
Imma-20 <immaculeeh46@gmail.com>
2024-12-09 01:03:08 +0100

    Notifications et Evenements

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Models;
-
+use App\Events\ChirpCreated;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -11,6 +12,10 @@ class Chirp extends Model
     protected $fillable = [
         'message',
     ];
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+ 
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
